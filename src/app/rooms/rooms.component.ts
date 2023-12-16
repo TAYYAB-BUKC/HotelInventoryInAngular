9ba97dd20e72598a1cfea5548de04dacd5ab3eae
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2023-12-16 07:47:46 +0500

    Life Cycle Hook "ngAfterViewInit" usage.

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,16 +1,17 @@
-import { Component, DoCheck, OnInit } from '@angular/core';
+import { AfterViewInit, Component, DoCheck, OnInit, ViewChild } from '@angular/core';
 import { IRoom, IRoomList } from './rooms';
 import { CommonModule } from '@angular/common';
 import { RoomListComponent } from './room-list/room-list.component';
+import { HeaderComponent } from '../header/header.component'
 
 @Component({
   selector: 'hinv-rooms',
   standalone: true,
-  imports: [CommonModule, RoomListComponent],
+  imports: [CommonModule, RoomListComponent, HeaderComponent],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.scss'
 })
-export class RoomsComponent implements OnInit, DoCheck {
+export class RoomsComponent implements OnInit, DoCheck, AfterViewInit {
   
   hotelDescription = "Welcome to our urban oasis, where luxury meets convenience in the heart of the city. Nestled in a vibrant neighborhood, our hotel is a beacon of sophistication and modern comfort. Immerse yourself in the elegant ambiance of our meticulously designed rooms, each thoughtfully appointed to cater to your every need. Whether you're here for business or leisure, our dedicated staff is committed to ensuring your stay is nothing short of extraordinary. Indulge in culinary delights at our exquisite restaurant, unwind in our state-of-the-art spa, or simply bask in the breathtaking views from our rooftop lounge. With our unparalleled blend of style and substance, your experience at our hotel will be nothing less than unforgettable. Welcome to a world where every moment is curated to perfection, making your stay more than just accommodation – it's an indulgence in the art of hospitality.";
 
@@ -90,12 +91,13 @@ export class RoomsComponent implements OnInit, DoCheck {
     rating: 5
   }
 
-
   roomsList: IRoomList[] = [];
 
   role: string = "SuperAdmin";
 
   title: string = "Room List";
+
+  @ViewChild(HeaderComponent) headerComponent!: HeaderComponent;
 
   ngOnInit(): void {
     this.roomsList = [
@@ -122,5 +124,9 @@ export class RoomsComponent implements OnInit, DoCheck {
 
   ngDoCheck(): void {
     console.log('DoCheck event is raised');
+  }
+
+  ngAfterViewInit(): void {
+    this.headerComponent.title = 'Rooms Header';
   }
 }
