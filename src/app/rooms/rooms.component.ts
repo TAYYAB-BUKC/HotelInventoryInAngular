e325f31d97bd487a77b90fbc2e22354847e6ae87
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2023-12-19 04:47:46 +0500

    Service Usage.

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -3,6 +3,7 @@ import { IRoom, IRoomList } from './rooms';
 import { CommonModule } from '@angular/common';
 import { RoomListComponent } from './room-list/room-list.component';
 import { HeaderComponent } from '../header/header.component'
+import { RoomService } from '../services/room.service';
 
 @Component({
   selector: 'hinv-rooms',
@@ -23,61 +24,6 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit, AfterCont
     totalRooms: 10,
     bookedRooms: 9,
     availableRooms: 1
-  }
-
-  room1: IRoomList = {
-    number: 1,
-    type: "Standard",
-    price: 100,
-    amenities: "Wi-Fi, TV",
-    photos: "room1.jpg",
-    checkinTime: new Date("2023-12-13T12:00:00"),
-    checkoutTime: new Date("2023-12-14T10:00:00"),
-    rating: 2
-  };
-
-  room2: IRoomList = {
-    number: 2,
-    type: "Deluxe",
-    price: 150,
-    amenities: "Wi-Fi, TV, Mini Bar",
-    photos: "room2.jpg",
-    checkinTime: new Date("2023-12-13T14:00:00"),
-    checkoutTime: new Date("2023-12-14T12:00:00"),
-    rating: 2.5
-  }
-
-  room3: IRoomList = {
-    number: 3,
-    type: "Suite",
-    price: 200,
-    amenities: "Wi-Fi, TV, Mini Bar, Jacuzzi",
-    photos: "room3.jpg",
-    checkinTime: new Date("2023-12-13T16:00:00"),
-    checkoutTime: new Date("2023-12-14T14:00:00"),
-    rating: 3
-  }
-
-  room4: IRoomList = {
-    number: 4,
-    type: "Standard",
-    price: 120,
-    amenities: "Wi-Fi, TV, Balcony",
-    photos: "room4.jpg",
-    checkinTime: new Date("2023-12-13T18:00:00"),
-    checkoutTime: new Date("2023-12-14T16:00:00"),
-    rating: 4
-  }
-
-  room5: IRoomList = {
-    number: 5,
-    type: "Deluxe",
-    price: 180,
-    amenities: "Wi-Fi, TV, Mini Bar, Sea View",
-    photos: "room5.jpg",
-    checkinTime: new Date("2023-12-13T20:00:00"),
-    checkoutTime: new Date("2023-12-14T18:00:00"),
-    rating: 4.5546
   }
 
   room6: IRoomList = {
@@ -103,10 +49,11 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit, AfterCont
   
   hideHeader = false;
 
+  constructor(private roomService: RoomService){
+
+  }
   ngOnInit(): void {
-    this.roomsList = [
-      this.room1, this.room2, this.room3, this.room4, this.room5
-    ]
+    this.roomsList = this.roomService.GetRooms();
   }
 
   ToggleRoomsInfo() {
---
+++ src/app/services/room.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { IRoomList } from '../rooms/rooms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomService {
+
+  constructor() { }
+
+  room1: IRoomList = {
+    number: 1,
+    type: "Standard",
+    price: 100,
+    amenities: "Wi-Fi, TV",
+    photos: "room1.jpg",
+    checkinTime: new Date("2023-12-13T12:00:00"),
+    checkoutTime: new Date("2023-12-14T10:00:00"),
+    rating: 2
+  };
+
+  room2: IRoomList = {
+    number: 2,
+    type: "Deluxe",
+    price: 150,
+    amenities: "Wi-Fi, TV, Mini Bar",
+    photos: "room2.jpg",
+    checkinTime: new Date("2023-12-13T14:00:00"),
+    checkoutTime: new Date("2023-12-14T12:00:00"),
+    rating: 2.5
+  }
+
+  room3: IRoomList = {
+    number: 3,
+    type: "Suite",
+    price: 200,
+    amenities: "Wi-Fi, TV, Mini Bar, Jacuzzi",
+    photos: "room3.jpg",
+    checkinTime: new Date("2023-12-13T16:00:00"),
+    checkoutTime: new Date("2023-12-14T14:00:00"),
+    rating: 3
+  }
+
+  room4: IRoomList = {
+    number: 4,
+    type: "Standard",
+    price: 120,
+    amenities: "Wi-Fi, TV, Balcony",
+    photos: "room4.jpg",
+    checkinTime: new Date("2023-12-13T18:00:00"),
+    checkoutTime: new Date("2023-12-14T16:00:00"),
+    rating: 4
+  }
+
+  room5: IRoomList = {
+    number: 5,
+    type: "Deluxe",
+    price: 180,
+    amenities: "Wi-Fi, TV, Mini Bar, Sea View",
+    photos: "room5.jpg",
+    checkinTime: new Date("2023-12-13T20:00:00"),
+    checkoutTime: new Date("2023-12-14T18:00:00"),
+    rating: 4.5546
+  }
+
+  roomsList: IRoomList[] = [
+    this.room1, this.room2, this.room3, this.room4, this.room5
+  ]
+
+  GetRooms() {
+    return this.roomsList;
+  }
+}
